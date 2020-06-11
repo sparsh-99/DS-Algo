c8a94d342e19d7f388474b605a5ed9a6af3ed08f
Sparsh Garg <GUPTA.ANMOL045@GMAIL.COM>
2020-06-12 00:16:46 +0530

    leftRotation_Array

---
+++ LeftRotation_array/src/leftArray.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class leftArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int d = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0;i<n;i++){
+            a[(i+n-d)%n] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
